51a7300638f85eff29b8b027a41c6ac995f9e71e
Priyansu Dey <deypriyansu41@gmail.com>
2026-08-16 22:30:04 +0530

    Add solution for Stone Game IX

--- LeetCode/Stone-Game-IX/Solution.java
+++ LeetCode/Stone-Game-IX/Solution.java
@@ -1,17 +1,8 @@
 1class Solution {
 2    public boolean stoneGameIX(int[] stones) {
-3        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
-4        for (int stone : stones) {
-5            int rem = stone % 3;
-6            if (rem == 0) cnt0++;
-7            else if (rem == 1) cnt1++;
-8            else cnt2++;
-9        }
-10        
-11        if (cnt0 % 2 == 0) {
-12            return cnt1 >= 1 && cnt2 >= 1;
-13        } else {
-14            return Math.abs(cnt1 - cnt2) > 2;
-15        }
-16    }
-17}
+3        int[] count=new int[3];
+4for(int stone:stones)count[stone%3]++;
+5if(count[0]%2==0)return count[1]>0&&count[2]>0;
+6return Math.abs(count[1]-count[2])>2;
+7    }
+8}
