1e3062802ef79f945e3acf97b3b1cfdf51f1abf4
Priyansu Dey <deypriyansu41@gmail.com>
2026-09-07 22:31:48 +0530

    Add solution for Spiral Matrix II

--- LeetCode/Spiral-Matrix-II/Solution.java
+++ LeetCode/Spiral-Matrix-II/Solution.java
@@ -10,35 +10,30 @@
 10        int num = 1;
 11        
 12        while (top <= bottom && left <= right) {
-13            // Traverse from left to right along the top row
-14            for (int i = left; i <= right; i++) {
-15                matrix[top][i] = num++;
-16            }
-17            top++;
-18            
-19            // Traverse from top to bottom along the right column
-20            for (int i = top; i <= bottom; i++) {
-21                matrix[i][right] = num++;
-22            }
-23            right--;
-24            
-25            // Traverse from right to left along the bottom row
-26            if (top <= bottom) {
-27                for (int i = right; i >= left; i--) {
-28                    matrix[bottom][i] = num++;
-29                }
-30                bottom--;
-31            }
-32            
-33            // Traverse from bottom to top along the left column
-34            if (left <= right) {
-35                for (int i = bottom; i >= top; i--) {
-36                    matrix[i][left] = num++;
-37                }
-38                left++;
-39            }
-40        }
-41        
-42        return matrix;
-43    }
-44}
+13            for (int i = left; i <= right; i++) {
+14                matrix[top][i] = num++;
+15            }
+16            top++;
+17    
+18            for (int i = top; i <= bottom; i++) {
+19                matrix[i][right] = num++;
+20            }
+21            right--;
+22            if (top <= bottom) {
+23                for (int i = right; i >= left; i--) {
+24                    matrix[bottom][i] = num++;
+25                }
+26                bottom--;
+27            }
+28            
+29            if (left <= right) {
+30                for (int i = bottom; i >= top; i--) {
+31                    matrix[i][left] = num++;
+32                }
+33                left++;
+34            }
+35        }
+36        
+37        return matrix;
+38    }
+39}
